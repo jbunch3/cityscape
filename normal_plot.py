6cd524a60135d1650d05f50bf30978f2aab0800d
Justin.Bunch <jcbunch3@gmail.com>
2022-11-18 22:18:49 +0100

    WIP: Updates for Mobile

--- normal_plot.py
+++ normal_plot.py
@@ -24,10 +24,10 @@ fig = ff.create_distplot([x1["destruction"], x1["old_per_km2"]], group_labels, b
                          colors=colors, show_rug=False)
 
 # Add title
-fig.update_layout(title_text='Normal Distribution Plot', title_x=0.5, title_y=.95)
+fig.update_layout(title_text='Normal Distribution Plot', title_x=0.5, title_y=1)
 
 fig.update_layout(
-    margin = {'l': 50, 'r': 50, 't': 50, 'b': 50},
+    margin = {'l': 50, 'r': 0, 't': 50, 'b': 50},
     xaxis=dict(
         showgrid=False,
         showline=False,
@@ -38,27 +38,34 @@ fig.update_layout(
         showline=False,
         zeroline=False,
     ),
-    paper_bgcolor='rgb(248, 248, 255)',
-    plot_bgcolor='rgb(248, 248, 255)',
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
     title_font_family="Courier New",
     title_font_color="black",
     title_font_size=25,
     font_family="Courier New",
     font_color="black",
-    autosize=False, 
-    width=1200, 
-    height=700,
+    # autosize=False, 
+    # width=1200, 
+    # height=700,
     legend=dict(
-        x=.8,
-        y=.98,
         traceorder="normal",
         font=dict(
             family="Courier New",
-            size=20,
+            size=15,
             color="black"
         ),
+        orientation="h",
+        yanchor="bottom",
+        y=.96,
+        xanchor="right",
+        x=1
     )
 )
 
+fig.for_each_xaxis(lambda x: x.update(showgrid=True, showline=True, linewidth=1, linecolor='rgba(0,0,0,0.3)', gridcolor='rgba(0,0,0,0.1)', rangemode = "tozero"))
+fig.for_each_yaxis(lambda x: x.update(showgrid=True, showline=True, linewidth=1, linecolor='rgba(0,0,0,0.3)', gridcolor='rgba(0,0,0,0.1)', rangemode = "tozero"))
 
-fig.show()
+
+py.plot(fig, filename = 'normDist1', auto_open=True)
+# fig.show()
